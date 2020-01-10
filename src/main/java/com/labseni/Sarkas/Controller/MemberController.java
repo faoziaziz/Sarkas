4b6aafa3179523cc032383488a71326d5c760e17
Aziz Faozi <admin@labseni.com>
2020-01-10 15:13:35 +0700

    take my henad

--- src/main/java/com/labseni/Sarkas/Controller/MemberController.java
+++ src/main/java/com/labseni/Sarkas/Controller/MemberController.java
@@ -2,23 +2,27 @@ package com.labseni.Sarkas.Controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.labseni.Sarkas.model.User;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
+import com.labseni.Sarkas.model.Member;
+import com.labseni.Sarkas.model.MemberRepository;
 
 @RestController
 @RequestMapping("/member")
 @Api(value="Sexy management System", description = "Sexy mantap")
 public class MemberController {
+	
+	@Autowired
+	private MemberRepository memberRepository;
 	
 	@ApiOperation(value = "get all member")
 	@GetMapping(path="/all")	
@@ -27,16 +31,16 @@ public class MemberController {
 		return "Hello Crot";
 	}
 	
-	/*
+	
 	@ApiOperation(value="Post method")
-	@PostMapping(path="/postcrot")
-	public User helloPost(
+	@PostMapping(path="/addmember")
+	public Member helloPost(
 			@ApiParam(value = "Employee object store in database table", required = true) 
-			@Valid @RequestBody User user){
+			@Valid @RequestBody Member member){
 		
-		return userRepository.save(user);
+		return memberRepository.save(member);
 	}
-	*/
+	
 	
 
 }
---
+++ src/main/java/com/labseni/Sarkas/model/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.labseni.Sarkas.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MemberRepository extends CrudRepository<Member, Integer> {
+	
+}
